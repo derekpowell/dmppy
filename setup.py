cafd4fb07c221031cccb014b2bb14eac46ce1723
derekpowell <derekpowll@gmail.com>
2016-11-16 13:59:31 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 22 11:21:07 2016
+
+@author: User
+"""
+
+from setuptools import setup
+
+setup(name='dmppy',
+      version='0.1',
+      description='A collection of functions I use (dmp)',
+      url='',
+      author='derek powell',
+      author_email='derekpowll@gmail.com',
+      license='MIT',
+      packages=['dmppy'],
+      zip_safe=False)

3f405832b069e424c97054d10b81efc349c19b7b
derekpowell <derekpowll@gmail.com>
2017-02-28 15:19:05 -0800

    updating version to 0.2.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Created on Sat Oct 22 11:21:07 2016
 from setuptools import setup
 
 setup(name='dmppy',
-      version='0.2.2',
+      version='0.2.3',
       description='A collection of functions I use (dmp)',
       url='https://github.com/derekpowell/dmppy',
       author='derek powell',

2716a2c13b4bc1f6ce1fb8cc2af34258633e4498
derekpowell <derekpowll@gmail.com>
2017-02-28 15:18:35 -0800

    updating pmap and parallel_process

--- dmppy/__init__.py
+++ dmppy/__init__.py
@@ -9,31 +9,35 @@ import csv_unicode_handler
 import read_write
 
 from tqdm import tqdm
-from concurrent.futures import ProcessPoolExecutor, as_completed
-from multiprocessing import cpu_count
-from itertools import partial
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
+from functools import partial
+from multiprocessing import Pool, cpu_count
+from multiprocessing.pool import ThreadPool
 
 def func_kwstar(kwargs,func):
     """Convert `f({"x":1,"y":2})` to `f(1,2)` call."""
     return func(**kwargs)
 
-def p_map(func, iterable, use_kwargs = False, n_jobs = -1):
+def pmap(func, iterable, threading = False, use_kwargs = False, n_jobs = -1):
     """
-    Parallelized map function. More readable than parallel_process.
+    Parallelized map function. More readable than parallel_process but without progress bar or
+    serial initialization for debugging.
 
     func (function): function taking 1 argument to apply to iterable
     iterable (iterable): list or iterable of objects to be modified by func
+    threading (bool, default = False): use multithreading (defaults to multiprocessing)
     use_kwargs (bool, default = False): treat iterable items as keyword dicts
-    n_jobs (int, default = -1): number of processes (-1 for # CPUs)
+    n_jobs (int, default = -1): number of processes (-1 for cpu_count)
 
     """
-
-    from multiprocessing import Pool, cpu_count
 
     if n_jobs == -1:
         n_jobs = cpu_count()
 
-    pool = Pool(n_jobs)
+    if threading == False:
+        pool = Pool(n_jobs)
+    elif threading == True:
+        pool = ThreadPool(n_jobs)
 
     if __name__=="__main__":
 
@@ -51,7 +55,7 @@ def p_map(func, iterable, use_kwargs = False, n_jobs = -1):
         return output
 
 
-def parallel_process(function, array, n_jobs=-1, use_kwargs=False, front_num=3, progressbar = False):
+def parallel_process(function, array, n_jobs=-1, use_kwargs=False, front_num=0, bar = False, threading=False):
     """
         A parallel version of the map function with a tqdm progress bar.
 
@@ -61,9 +65,10 @@ def parallel_process(function, array, n_jobs=-1, use_kwargs=False, front_num=3, 
             n_jobs (int, default= -1): The number of cores to use (-1 = use all cores)
             use_kwargs (boolean, default=False): Whether to consider the elements of array as dictionaries of
                 keyword arguments to function
-            front_num (int, default=3): The number of iterations to run serially before kicking off the parallel job.
+            front_num (int, default=0): The number of iterations to run serially before kicking off the parallel job.
                 Useful for catching bugs
-            progressbar (bool, default=False): Show tqdm progress bar
+            bar (bool, default=False): Show tqdm progress bar
+            threading (bool, default = False): use multithreading (defaults to multiprocessing)
         Returns:
             [function(array[0]), function(array[1]), ...]
     """
@@ -76,8 +81,9 @@ def parallel_process(function, array, n_jobs=-1, use_kwargs=False, front_num=3, 
     if front_num > 0:
         front = [function(**a) if use_kwargs else function(a) for a in array[:front_num]]
     #If we set n_jobs to 1, just run a list comprehension. This is useful for benchmarking and debugging.
-
-    if progressbar == True:
+    else:
+        front = []
+    if bar == True:
         if n_jobs==1:
             return front + [function(**a) if use_kwargs else function(a) for a in tqdm(array[front_num:])]
         #Assemble the workers
@@ -107,18 +113,32 @@ def parallel_process(function, array, n_jobs=-1, use_kwargs=False, front_num=3, 
         if n_jobs==1:
             return front + [function(**a) if use_kwargs else function(a) for a in array[front_num:]]
         #Assemble the workers
-        with ProcessPoolExecutor(max_workers=n_jobs) as pool:
-            #Pass the elements of array into function
-            if use_kwargs:
-                futures = [pool.submit(function, **a) for a in array[front_num:]]
-            else:
-                futures = [pool.submit(function, a) for a in array[front_num:]]
-            kwargs = {
-                'total': len(futures),
-                'unit': 'it',
-                'unit_scale': True,
-                'leave': True
-            }
+        if threading == False:
+            with ProcessPoolExecutor(max_workers=n_jobs) as pool:
+                #Pass the elements of array into function
+                if use_kwargs:
+                    futures = [pool.submit(function, **a) for a in array[front_num:]]
+                else:
+                    futures = [pool.submit(function, a) for a in array[front_num:]]
+                kwargs = {
+                    'total': len(futures),
+                    'unit': 'it',
+                    'unit_scale': True,
+                    'leave': True
+                }
+        elif threading == True:
+            with ThreadPoolExecutor(max_workers=n_jobs) as pool:
+                #Pass the elements of array into function
+                if use_kwargs:
+                    futures = [pool.submit(function, **a) for a in array[front_num:]]
+                else:
+                    futures = [pool.submit(function, a) for a in array[front_num:]]
+                kwargs = {
+                    'total': len(futures),
+                    'unit': 'it',
+                    'unit_scale': True,
+                    'leave': True
+                }
 
         out = []
         #Get the results from the futures.
